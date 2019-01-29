25727e519d97474775618e5cc185a2f8f6d17e53
itevg <shiba.zheka65@gmail.com>
2019-01-29 18:34:46 +0500

    Add files via upload

--- NN.h
+++ NN.h
@@ -1,6 +1,6 @@
 //Autor -dreamIIx
 //GitHub - https://github.com/dreamIIx
-//Release on GitHub 27.01.2019
+//Release on GitHub 27.09.2018
 
 #include <iostream>
 #include <string>
@@ -8,42 +8,17 @@
 #include <math.h>
 #include <vector>
 #include <Windows.h>
+//#include <ctime>
 
 namespace nndx
 {
 	inline int randT();
 
-	struct dy_tpl
-	{
-		std::vector<int> tempDATA;
-
-		dy_tpl(int size, ...)
-		{
-			va_list args;
-			va_start(args, size);
-
-			tempDATA.reserve(size);
-			for (int i = 0; i < size; ++i)
-			{
-				int value = va_arg(args, int);
-				tempDATA.emplace_back(value);
-			}
-			va_end(args);
-		}
-
-		~dy_tpl()
-		{
-			tempDATA.clear();
-			//std::cout << "called ~dy_tpl()" << std::endl;
-		}
-	};
-
 	class neuron
 	{
 	public:
-		double data = 0.0;
-		double prevdata = 0.0;
-		double funcBP = 0.0;
+		double data = 0.0f;
+		double funcBP = 0.0f;
 		bool BIAS = false;
 		neuron(double num) : data(num) {}
 		void goF() { funcBP = data * (1 - data); }
@@ -54,9 +29,9 @@ namespace nndx
 	class wWw
 	{
 	public:
-		double wg = 0.0;
-		double dwg = 0.0;
-		double grad = 0.0;
+		double wg = 0.0f;
+		double dwg = 0.0f;
+		double grad = 0.0f;
 		wWw(double num) : wg(num) {}
 		wWw() {}
 	};
@@ -67,7 +42,6 @@ namespace nndx
 	class neuronet
 	{
 	public:
-		bool BIfunc = false;
 		double moment = 0.0; // 0.05
 		double u = 0.0; // 0.1
 		std::vector<dataA> data;
@@ -78,13 +52,12 @@ namespace nndx
 
 		neuronet() {}
 
-		neuronet(dy_tpl& temp, bool setBIfunc) : BIfunc(setBIfunc)
+		neuronet(std::vector<int> topology)
 		{
 			/// \<- SPECinit()
-			int* pos = temp.tempDATA.data();
-			for (int i = 0; i < temp.tempDATA.size(); i++)
+			for (int i = 0; i < topology.size(); i++)
 			{
-				int a = *pos++;
+				int a = topology[i];
 				if (a > 0)
 				{
 					data.push_back(dataA());
@@ -97,33 +70,25 @@ namespace nndx
 				}
 			}
 
-			for (size_t i = 0; i < data.size() - 1; i++)
+			for (int i = 0; i < data.size() - 1; i++)
 			{
 				data[i].push_back(1);
 				data[i].back().is_bias();
 			}
 
-			for (size_t i = 0; i < data.size() - 2; i++)
+			for (int i = 0; i < data.size() - 1; i++)
 			{
 				weight.push_back(dataW());
-				for (size_t j = 0; j < (data[i].size() * data[i + 1].size()) - data[i].size(); j++)
+				for (int j = 0; j < data[i].size() * data[i + 1].size(); j++)
 				{
 					weight.back().push_back((nndx::randT() % 6) - 2);
+					//weight.back().push_back((randT() % 101) * 0.01);
+					//weight.back().push_back(randT() * 0.001);
+					//weight.back().push_back(rand() / double(RAND_MAX));
+					//std::cout << weight.back().back().wg << "   ";
 				}
+				//std::cout << std::endl;
 			}
-			weight.push_back(dataW());
-			for (size_t j = 0; j < data[data.size() - 2].size() * data.back().size(); j++)
-			{
-				weight.back().push_back((nndx::randT() % 6) - 2);
-			}
-
-			/*for (size_t i = 0; i < weight.size(); ++i)
-			{
-				std::cout << weight[i].size() << " ";
-			}
-			std::cout << std::endl;*/
-
-			//temp.~dy_tpl();
 		}
 
 		void saveF(std::string s)
@@ -131,15 +96,15 @@ namespace nndx
 			std::cout << "Outputing weights..." << std::endl;
 
 			std::ofstream f("output/" + s);
-			for (size_t i = 0; i < topology_save.size(); i++)
+			for (int i = 0; i < topology_save.size(); i++)
 			{
 				f << topology_save[i] << " ";
 			}
 			f << "0" << std::endl;
 
-			for (size_t i = 0; i < weight.size(); i++)
+			for (int i = 0; i < weight.size(); i++)
 			{
-				for (size_t j = 0; j < weight[i].size(); j++)
+				for (int j = 0; j < weight[i].size(); j++)
 				{
 					f << weight[i][j].wg << std::endl;
 					f << weight[i][j].dwg << std::endl;
@@ -151,7 +116,7 @@ namespace nndx
 			std::cout << "Weights is saved!(File - " << s << ")" << std::endl;
 		}
 
-		/*void mA()
+		void mA()
 		{
 			std::ifstream read(nameT);
 			if (!read.is_open())
@@ -198,7 +163,7 @@ namespace nndx
 				/*for (int i = 0; i < weight.size(); i++)
 				{
 					std::cout << weight[i][weight[i].size() - 1].wg << std::endl;
-				}
+				}*/
 				std::vector<double> errDat;
 				for (int i = 0; i < data.back().size(); i++)
 				{
@@ -212,23 +177,58 @@ namespace nndx
 				std::cout << "------------" << std::endl;
 			}
 			read.close();
-		}*/
+		}
 
-		void SPECmA(std::vector<int>& dataT)
+		void SPECmA(double dx, double dy)
 		{
-			if (dataT.size() == data[0].size() - 1)
+			//for (int i = 0; i < data[0].size() - 1; i++)
+			//{
+			data[0][0].data = dx;
+			data[0][1].data = dy;
+			//if (!data[0][0].BIAS)	data[0][0].goF();
+			//}
+			activationF_BI();
+			/*for (int i = 0; i < data.back().size(); i++)
 			{
-				for (size_t i = 0; i < data[0].size() - 1; i++)
+				std::cout << data.back()[i].data << std::endl;
+			}*/
+		}
+
+		void SPECinit(std::vector<int> topology)
+		{
+			for (int i = 0; i < topology.size(); i++)
+			{
+				int a = topology[i];
+				if (a > 0)
 				{
-					data[0][i].data = dataT[i];
-					if (!data[0][i].BIAS)	data[0][i].goF();
-					else std::cout << "checkout here!" << std::endl;
+					data.push_back(dataA());
+
+					for (int i = 0; i < a; i++)
+					{
+						data.back().push_back(0);
+					}
+					topology_save.push_back(a);
 				}
-				activationF();
 			}
-			else
+
+			for (int i = 0; i < data.size() - 1; i++)
 			{
-				std::cout << "ERROR || !@#$" << std::endl;
+				data[i].push_back(1);
+				data[i].back().is_bias();
+			}
+
+			for (int i = 0; i < data.size() - 1; i++)
+			{
+				weight.push_back(dataW());
+				for (int j = 0; j < data[i].size() * data[i + 1].size(); j++)
+				{
+					weight.back().push_back((nndx::randT() % 6) - 2);
+					//weight.back().push_back((randT() % 101) * 0.01);
+					//weight.back().push_back(randT() * 0.001);
+					//weight.back().push_back(rand() / double(RAND_MAX));
+					//std::cout << weight.back().back().wg << "   ";
+				}
+				//std::cout << std::endl;
 			}
 		}
 
@@ -250,6 +250,7 @@ namespace nndx
 
 						data.push_back(dataA());
 
+						//srand(unsigned(time(NULL)));
 						for (int i = 0; i < *a; i++)
 						{
 							data.back().push_back(0);
@@ -259,24 +260,19 @@ namespace nndx
 				}
 				delete a;
 
-				for (size_t i = 0; i < data.size() - 1; i++)
+				for (int i = 0; i < data.size() - 1; i++)
 				{
 					data[i].push_back(1);
 					data[i].back().is_bias();
 				}
 
-				for (size_t i = 0; i < data.size() - 2; i++)
+				for (int i = 0; i < data.size() - 1; i++)
 				{
 					weight.push_back(dataW());
-					for (size_t j = 0; j < (data[i].size() * data[i + 1].size()) - data[i].size(); j++)
+					for (int j = 0; j < data[i].size() * data[i + 1].size(); j++)
 					{
-						weight.back().push_back((nndx::randT() % 6) - 2);
+						weight.back().push_back(randT() * 0.001);
 					}
-				}
-				weight.push_back(dataW());
-				for (size_t j = 0; j < data[data.size() - 2].size() * data.back().size(); j++)
-				{
-					weight.back().push_back((nndx::randT() % 6) - 2);
 				}
 				std::cout << "Training file(default): " << nameT << std::endl;
 				std::cout << "SUCCESS! Press anykey to start..." << std::endl;
@@ -307,16 +303,16 @@ namespace nndx
 				delete a;
 				double w;
 
-				for (size_t i = 0; i < data.size() - 1; i++)
+				for (int i = 0; i < data.size() - 1; i++)
 				{
 					data[i].push_back(1);
 					data[i].back().is_bias();
 				}
 				std::cout << "Init weights from file..." << std::endl;
-				for (size_t i = 0; i < data.size() - 2; i++)
+				for (int i = 0; i < data.size() - 1; i++)
 				{
 					weight.push_back(dataW());
-					for (size_t j = 0; j < (data[i].size() * data[i + 1].size()) - data[i].size(); j++)
+					for (int j = 0; j < data[i].size() * data[i + 1].size(); j++)
 					{
 						read >> w;
 						weight.back().push_back(w);
@@ -334,24 +330,6 @@ namespace nndx
 						system("pause"); // after this moment, need to close programm
 					}
 				}
-				weight.push_back(dataW());
-				for (size_t j = 0; j < data[data.size() - 2].size() * data.back().size(); j++)
-				{
-					read >> w;
-					weight.back().push_back(w);
-					std::cout << w << std::endl;
-					read >> w;
-					weight.back().back().dwg = w;
-					std::cout << w << std::endl;
-				}
-				std::string check;
-				read >> check;
-				if (check != "-----")
-				{
-					std::cout << "check = " << check << std::endl;
-					std::cout << "ERROR! CHECKOUT!" << std::endl;
-					system("pause"); // after this moment, need to close programm
-				}
 				read >> nameT;
 				read.close();
 				std::cout << "Name of training file: " << nameT << " (check)" << std::endl;
@@ -364,110 +342,74 @@ namespace nndx
 		{
 			double local_sum = 0;
 
-			for (size_t i = 1; i < data.size() - 1; i++)
+			for (int i = 1; i < data.size(); i++)
 			{
-				std::cout << "----------------  data.size - " << data.size() << "   now - " << i << std::endl;
-				for (size_t j = 0; j < data[i].size(); j++)
+				for (int j = 0; j < data[i].size(); j++)
 				{
-					std::cout << "----------------  data[" << i <<  "].size - " << data[i].size() << "  now - " << j << "   ";
-					if(!data[i][j].BIAS)
+					local_sum = 0;
+					for (int prev = 0; prev < data[i - 1].size(); prev++)
 					{
-						std::cout << "- is not BIAS | ";
-						local_sum = 0;
-						for (size_t prev = 0; prev < data[i - 1].size(); prev++)
-						{
-							std::cout << prev * (data[i].size() - 1) + j << " ";
-							local_sum += data[i - 1][prev].data * weight[i - 1][prev * (data[i].size() - 1) + j].wg;
-						}
-						std::cout << std::endl;
-						if (!BIfunc)
-						{
-							data[i][j].data = 1 / (1 + exp(-local_sum));
-							data[i][j].goF();
-						}
-						else
-						{
-							data[i][j].data = tanh(local_sum);
-							data[i][j].goF_BI();
-						}
+						local_sum += data[i - 1][prev].data * weight[i - 1][prev * data[i].size() + j].wg;
 					}
-					else std::cout << "- is BIAS | " << std::endl;
+					if (!data[i][j].BIAS)	data[i][j].data = 1 / (1 + exp(-local_sum));
+					if (!data[i][j].BIAS)	data[i][j].goF();
 				}
 			}
-			std::cout << "----------------  data.size - " << data.size() << "   now - last" << std::endl;
-			for (size_t j = 0; j < data.back().size(); j++)
+		}
+
+		void activationF_BI()
+		{
+			double local_sum = 0;
+
+			for (int i = 1; i < data.size(); i++)
 			{
-				std::cout << "----------------  data[last].size - " << data[data.size() - 1].size() << "  now - " << j << "   ";
-				if (!data.back()[j].BIAS)
+				for (int j = 0; j < data[i].size(); j++)
 				{
-					std::cout << "- is not BIAS | ";
 					local_sum = 0;
-					for (size_t prev = 0; prev < data[data.size() - 2].size(); prev++)
+					for (int prev = 0; prev < data[i - 1].size(); prev++)
 					{
-						std::cout << prev * data.back().size() + j << " ";
-						local_sum += data[data.size() - 2][prev].data * weight[data.size() - 2][prev * data.back().size() + j].wg;
+						local_sum += data[i - 1][prev].data * weight[i - 1][prev * data[i].size() + j].wg;
 					}
-					std::cout << std::endl;
-					if (!BIfunc)
-					{
-						data.back()[j].data = 1 / (1 + exp(-local_sum));
-						data.back()[j].goF();
-					}
-					else
-					{
-						data.back()[j].data = tanh(local_sum);
-						data.back()[j].goF_BI();
-					}
+					if (!data[i][j].BIAS)	data[i][j].data = tanh(local_sum);
+					if (!data[i][j].BIAS)	data[i][j].goF_BI();
 				}
-				else std::cout << "- is BIAS | " << std::endl;
 			}
 		}
 
 		void backProp(std::vector<double> &d)
 		{
-			typedef std::vector<double> dw;
-			std::vector<dw> errR;
+			std::vector<dataW> errR;
 
-			for (size_t i = 0; i < data.size(); i++)
+			for (int i = 0; i < data.size(); i++)
 			{
-				errR.push_back(dw());
+				errR.push_back(dataW());
 			}
 
-			for (size_t i = 0; i < data.back().size(); i++)
+			for (int i = 0; i < data.back().size(); i++)
 			{
 				errR.back().push_back((d[i] - data.back()[i].data) * data.back()[i].funcBP);
 			}
-			
+
 			double local_sum = 0;
-			for (size_t j = 0; j < data[data.size() - 2].size(); j++)
-			{
-				local_sum = 0;
-				for (size_t next = 0; next < data[data.size() - 1].size(); next++)
-				{
-					local_sum += errR[data.size() - 1][next] * weight[data.size() - 2][(data[data.size() - 1].size() - 1) * j + next].wg;
-					weight[data.size() - 2][(data[data.size() - 1].size() - 1) * j + next].grad = errR[data.size() - 1][next] * data[data.size() - 2][j].data;
-				}
-				errR[data.size() - 2].push_back(local_sum * data[data.size() - 2][j].funcBP);
-			}
 			for (size_t i = data.size() - 2; i >= 0; i--)
 			{
-				for (size_t j = 0; j < data[i].size(); j++)
+				for (int j = 0; j < data[i].size(); j++)
 				{
 					local_sum = 0;
-					for (size_t next = 0; next < data[i + 1].size(); next++)
+					for (int next = 0; next < data[i + 1].size(); next++)
 					{
-						local_sum += errR[i + 1][next] * weight[i][(data[i + 1].size() - 1) * j + next].wg;
-						weight[i][(data[i + 1].size() - 1) * j + next].grad = errR[i + 1][next] * data[i][j].data;
+						local_sum += errR[i + 1][next].wg * weight[i][data[i + 1].size() * j + next].wg;
+						weight[i][data[i + 1].size() * j + next].grad = errR[i + 1][next].wg * data[i][j].data;
 					}
 					errR[i].push_back(local_sum * data[i][j].funcBP);
 				}
 			}
 
-			for (size_t i = 1; i < data.size(); i++)
+			for (int i = 1; i < data.size(); i++)
 			{
-				for (size_t j = 0; j < data[i].size(); j++)
+				for (int j = 0; j < data[i].size(); j++)
 				{
-					for (size_t prev = 0; prev < data[i - 1].size(); prev++)
+					for (int prev = 0; prev < data[i - 1].size(); prev++)
 					{
 						weight[i - 1][prev * data[i].size() + j].dwg = u * weight[i - 1][prev * data[i].size() + j].grad + (moment * weight[i - 1][prev * data[i].size() + j].dwg);
 						weight[i - 1][prev * data[i].size() + j].wg += weight[i - 1][prev * data[i].size() + j].dwg;
@@ -477,77 +419,29 @@ namespace nndx
 		}
 	};
 
-	/*void funcHebb()
-	{
-
-	}*/
-
-	int randT()
+	inline int randT()
 	{
 		HCRYPTPROV hProv;
+		HCRYPTPROV *phProv = &hProv;
 
 		BYTE Buf1;
 		BYTE Buf2;
 
-		/*retval = CryptAcquireContextW(&hProv, 0, 0, PROV_RSA_FULL, 0);
+		BOOL retval;
+		retval = CryptAcquireContext(phProv, 0, 0, PROV_RSA_FULL, 0);
+
 		if (retval != 0)
 		{
 			CryptGenRandom(hProv, DWORD(sizeof(BYTE)), &Buf1);
 			CryptReleaseContext(hProv, 0);
-		}*/
+		}
+		retval = CryptAcquireContext(phProv, 0, 0, PROV_RSA_FULL, 0);
 
-		if (!CryptAcquireContext(&hProv, 0, NULL, PROV_RSA_FULL, 0))
-		{
-			if (GetLastError() == NTE_BAD_KEYSET)
-			{
-				if (!CryptAcquireContext(&hProv, 0, NULL, PROV_RSA_FULL, CRYPT_NEWKEYSET))
-				{
-					// Error ...
-					std::cout << "Error ... [crypt]" << std::endl;
-					system("pause");
-				}
-				else
-				{
-					CryptGenRandom(hProv, DWORD(sizeof(BYTE)), &Buf1);
-					CryptReleaseContext(hProv, 0);
-				}
-			}
-		}
-		else
-		{
-			CryptGenRandom(hProv, DWORD(sizeof(BYTE)), &Buf1);
-			CryptReleaseContext(hProv, 0);
-		}
-
-		if (!CryptAcquireContext(&hProv, 0, NULL, PROV_RSA_FULL, 0))
-		{
-			if (GetLastError() == NTE_BAD_KEYSET)
-			{
-				if (!CryptAcquireContext(&hProv, 0, NULL, PROV_RSA_FULL, CRYPT_NEWKEYSET))
-				{
-					// Error ...
-					std::cout << "Error ... [crypt]" << std::endl;
-					system("pause");
-				}
-				else
-				{
-					CryptGenRandom(hProv, DWORD(sizeof(BYTE)), &Buf2);
-					CryptReleaseContext(hProv, 0);
-				}
-			}
-		}
-		else
-		{
-			CryptGenRandom(hProv, DWORD(sizeof(BYTE)), &Buf2);
-			CryptReleaseContext(hProv, 0);
-		}
-
-		/*retval = CryptAcquireContextW(phProv, 0, 0, PROV_RSA_FULL, 0);
 		if (retval != 0)
 		{
 			CryptGenRandom(hProv, DWORD(sizeof(BYTE)), &Buf2);
 			CryptReleaseContext(hProv, 0);
-		}*/
+		}
 
 		int i = (int)Buf1;
 		i += (int)Buf2;

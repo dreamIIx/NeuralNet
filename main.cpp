95553be7ce206c8117f7501b8294581854ea549c
-dreamIIx <7871388+dreamIIx@users.noreply.github.com>
2019-02-17 19:46:02 +0500

    actual v2.0 (+ thread)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <thread>
 #include <vector>
 #include "SFML/Graphics.hpp"
 
@@ -13,22 +14,37 @@
 #define ERROR_ ::std::cout << def__FILELINE << ::std::endl;
 #endif
 
+#ifndef def_XTT_fast_ifstream
+#define def_XTT_fast_ifstream
+::std::string def_FIs(::std::ifstream& read_cont__)
+{
+	::std::string temp_string__;
+	read_cont__ >> temp_string__;
+	return temp_string__;
+}
+int def_FIi(::std::ifstream& read_cont__)
+{
+	int temp_int__;
+	read_cont__ >> temp_int__;
+	return temp_int__;
+}
+#endif
+
 #include "NN.h"
 #include "MainH.h"
 
-#define WIN_X 1000
-#define WIN_Y 400
-#define _SIZE_ 12
-#define _NUM_ACTIVE_WALL 4
-#define FILEIMAGE "files/image.png"
-#define TEXTURE_OBJ_X 20
-#define TEXTURE_OBJ_Y 20
-#define SIZE_VECTOR_WALL (WIN_X / TEXTURE_OBJ_X) + 1
-#define POSX 410
-#define POSY 200
-#define POSY_WALL 140
-#define KF 0.6
-#define TIME 2000
+#define def_WIN_X 1000
+#define def_WIN_Y 400
+#define def__SIZE_ 12
+#define def__NUM_ACTIVE_WALL 4
+#define def_FILEIMAGE "files/image.png"
+#define def_TEXTURE_OBJ_X 20
+#define def_TEXTURE_OBJ_Y 20
+#define def_SIZE_VECTOR_WALL (def_WIN_X / def_TEXTURE_OBJ_X) + 1
+#define def_POSX 410
+#define def_POSY 200
+#define def_POSY_WALL 140
+#define def_KF 0.6
 
 
 class Object
@@ -42,19 +58,19 @@ public:
 	Object(sf::Texture& t, const nndx::dy_tpl& tpl, int i) : obj(t), net(tpl, true), score(0), life(true), i(i)
 	{
 		//if (i != 0)	net.SPECinit(topology);
-		obj.setTextureRect(sf::IntRect(0, TEXTURE_OBJ_Y * i, TEXTURE_OBJ_X, TEXTURE_OBJ_Y));
-		obj.setPosition(static_cast<float>(POSX), static_cast<float>(POSY));
-		obj.setOrigin(sf::Vector2f(TEXTURE_OBJ_X / 2, TEXTURE_OBJ_Y / 2));
+		obj.setTextureRect(sf::IntRect(0, def_TEXTURE_OBJ_Y * i, def_TEXTURE_OBJ_X, def_TEXTURE_OBJ_Y));
+		obj.setPosition(static_cast<float>(def_POSX), static_cast<float>(def_POSY));
+		obj.setOrigin(sf::Vector2f(def_TEXTURE_OBJ_X / 2.0f, def_TEXTURE_OBJ_Y / 2.0f));
 	}
 
 	void mA(::std::vector<double>& args)
 	{
-		if (args[0] > KF)
+		if (args[0] > def_KF)
 		{
 			obj.move(20.0f, 0.0f);
 			score += 20;
 		}
-		if (args[1] > KF)
+		if (args[1] > def_KF)
 		{
 			if (obj.getPosition().y < 240)
 			{
@@ -62,7 +78,7 @@ public:
 				score += 5;
 			}
 		}
-		else if(args[1] < -KF)
+		else if(args[1] < -def_KF)
 		{
 			if (obj.getPosition().y > 140)
 			{
@@ -80,76 +96,134 @@ public:
 
 	Wall(sf::Texture& t, float x, float y) : wall(t)
 	{
-		wall.setTextureRect(sf::IntRect(TEXTURE_OBJ_X, 0, TEXTURE_OBJ_X, TEXTURE_OBJ_Y));
+		wall.setTextureRect(sf::IntRect(def_TEXTURE_OBJ_X, 0, def_TEXTURE_OBJ_X, def_TEXTURE_OBJ_Y));
 		wall.setPosition(x, y);
-		wall.setOrigin(sf::Vector2f(TEXTURE_OBJ_X / 2, TEXTURE_OBJ_Y / 2));
+		wall.setOrigin(sf::Vector2f(def_TEXTURE_OBJ_X / 2.0f, def_TEXTURE_OBJ_Y / 2.0f));
 	}
 };
 
-void radixSort(::std::vector<Object*>& a);
-void mA_gen(::std::vector<Object*>& obj, const int& tg);
-void mT(::std::vector<nndx::dataW>& v, size_t index1, size_t index2);
+void radixSort(::std::vector<Object*>&);
+void mA_gen(::std::vector<Object*>&, const int&);
+void mT(::std::vector<nndx::dataW>&, size_t, size_t);
+void mainA(sf::RenderWindow&, ::std::vector<Object>&, ::std::vector<::std::vector<Wall>>&, ::std::vector<int>&, sf::Texture&, sf::Int32&, bool&);
 
 int main()
 {
-	sf::RenderWindow win({ WIN_X, WIN_Y }, "test");
-	//win.setFramerateLimit(2);
+	::std::ifstream _tempRead("inf.cfg");
+	sf::Int32 TIMESET_;
+	sf::RenderWindow win({ def_WIN_X, def_WIN_Y }, "NN");
+	win.setFramerateLimit(def_FIi(_tempRead));
 	win.setVerticalSyncEnabled(true);
+	win.setActive(false);
+	TIMESET_ = static_cast<sf::Int32>(def_FIi(_tempRead));
+	_tempRead.close();
 
-	sf::Image image;
-	image.loadFromFile(FILEIMAGE);
-	sf::Texture t;
-	t.loadFromImage(image);
+	sf::Image image_;
+	image_.loadFromFile(def_FILEIMAGE);
+	sf::Texture t_;
+	t_.loadFromImage(image_);
 
-	::std::vector<Object> v_Obj;
-	v_Obj.reserve(_SIZE_);
-	//v_Obj.emplace_back(t, topology__, 0);
-	//v_Obj[0].net.nameF = "wWWw_OUT";
-	//v_Obj[0].net.init();
-	for (int i = 0; i < _SIZE_; ++i)
-	{
-		v_Obj.emplace_back(t, nndx::dy_tpl(3, 2, 3, 2), i);
-	}
-	::std::vector<::std::vector<Wall>> v_Wll;
-	::std::vector<int> target;
-	v_Wll.reserve(SIZE_VECTOR_WALL);
-	target.reserve(SIZE_VECTOR_WALL);
-	int tmpy = nndx::randT() % 4;
-	int next = nndx::randT() % 4;
-	float wposx = 470.0f;
-	target.emplace_back(POSY_WALL + 20 + tmpy * 20);
-	v_Wll.push_back(::std::vector<Wall>());
-	v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL));
-	for (int i = 0; i < _NUM_ACTIVE_WALL; ++i)
-	{
-		if ((i < (tmpy < next ? tmpy : next)) || (i > (tmpy > next ? tmpy : next)))	v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL + 20 + i * 20));
-	}
-	v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL + 100));
-	tmpy = next;
-	wposx += 20.0f;
+	::std::vector<Object> v_Obj_;
+	::std::vector<::std::vector<Wall>> v_Wll_;
+	::std::vector<int> target_;
+	v_Obj_.reserve(def__SIZE_);
+	v_Wll_.reserve(def_SIZE_VECTOR_WALL);
+	target_.reserve(def_SIZE_VECTOR_WALL);
 
-	sf::View view;
-	view.reset(sf::FloatRect(0.0f, 0.0f, WIN_X, WIN_Y));
-	dx::TimeGet tm;
-	size_t bufsizeO;
+	bool isOpen = true;
 
-	int mI = -1;
-	int mP = -1;
+	::std::thread mainThread(mainA, ::std::ref(win), ::std::ref(v_Obj_), ::std::ref(v_Wll_), ::std::ref(target_), ::std::ref(t_), ::std::ref(TIMESET_), ::std::ref(isOpen));
 
-	while (win.isOpen())
+	while (isOpen)
 	{
 		sf::Event event;
-		while (win.pollEvent(event))
+		while (win.waitEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 			{
-				win.close();
+				isOpen = false;
 				break;
 			}
 		}
+	}
 
+	mainThread.join();
+	win.setActive(true);
+	win.close();
+
+	for (int i = 0; i < def__SIZE_; ++i)
+	{
+		::std::string s = "output/net_";
+		char ch;
+		if (i < 10)
+		{
+			ch = '0' + i;
+			s += ch;
+			s += ".txt";
+		}
+		else
+		{
+			s += '1';
+			ch = '0' + (i % 10);
+			s += ch;
+			s += ".txt";
+		}
+		v_Obj_[i].net.saveF(s);
+	}
+
+	::std::ofstream out("output/log.txt");
+	for (int i = 0; i < def__SIZE_; ++i)
+	{
+		out << "net_" << i << " - " << v_Obj_[i].score << ::std::endl;
+	}
+	out.close();
+
+	return 0;
+}
+
+void mainA
+(
+	sf::RenderWindow& win,
+	::std::vector<Object>& v_Obj,
+	::std::vector<::std::vector<Wall>>& v_Wll,
+	::std::vector<int>& target,
+	sf::Texture& t,
+	sf::Int32& TIMESET,
+	bool& is_Open
+)
+{
+	win.setActive(true);
+	sf::View view;
+	view.reset(sf::FloatRect(0.0f, 0.0f, static_cast<float>(def_WIN_X), static_cast<float>(def_WIN_Y)));
+
+	for (int i = 0; i < def__SIZE_; ++i)
+	{
+		v_Obj.emplace_back(t, nndx::dy_tpl(3, 2, 3, 2), i);
+	}
+
+	int tmpy = nndx::randT() % 4;
+	int next = nndx::randT() % 4;
+	float wposx = 470.0f;
+
+	target.emplace_back(def_POSY_WALL + 20 + tmpy * 20);
+	v_Wll.emplace_back(::std::vector<Wall>());
+	v_Wll.back().reserve(6);
+	v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL));
+	for (int i = 0; i < def__NUM_ACTIVE_WALL; ++i)
+	{
+		if ((i < (tmpy < next ? tmpy : next)) || (i > (tmpy > next ? tmpy : next)))	v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL + 20 + i * 20));
+	}
+	v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL + 100));
+	tmpy = next;
+	wposx += 20.0f;
+
+	size_t bufsizeO;
+	dx::TimeGet tm;
+
+	while (is_Open)
+	{
 		::std::vector<Object*> _ptr;
-		for (int i = 0; i < _SIZE_; ++i)
+		for (int i = 0; i < def__SIZE_; ++i)
 		{
 			if (v_Obj[i].life)	_ptr.emplace_back(&v_Obj[i]);
 		}
@@ -170,21 +244,22 @@ int main()
 		}
 		if (static_cast<int>(wposx) - maxposx_obj < 100)
 		{
-			v_Wll.push_back(::std::vector<Wall>());
-			v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL));
+			v_Wll.emplace_back(::std::vector<Wall>());
+			v_Wll.back().reserve(6);
+			v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL));
 			next = nndx::randT() % 4;
-			target.emplace_back(POSY_WALL + 20 + tmpy * 20);
-			for (int i = 0; i < _NUM_ACTIVE_WALL; ++i)
+			target.emplace_back(def_POSY_WALL + 20 + tmpy * 20);
+			for (int i = 0; i < def__NUM_ACTIVE_WALL; ++i)
 			{
-				if ((i < (tmpy < next ? tmpy : next)) || (i > (tmpy > next ? tmpy : next)))	v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL + 20 + i * 20));
+				if ((i < (tmpy < next ? tmpy : next)) || (i > (tmpy > next ? tmpy : next)))	v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL + 20 + i * 20));
 			}
-			v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL + 100));
+			v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL + 100));
 			tmpy = next;
 			wposx += 20.0f;
 		}
 
 		bufsizeO = _ptr.size();
-		if ((_ptr.size()) && (bufsizeO == _ptr.size() ? *tm < TIME : 1))
+		if ((_ptr.size()) && (bufsizeO == _ptr.size() ? *tm < TIMESET : 1))
 		{
 			for (size_t i = 0; i < _ptr.size(); ++i)
 			{
@@ -223,37 +298,33 @@ int main()
 		}
 		else
 		{
-			for (int i = 0; i < _SIZE_; ++i)
+			for (int i = 0; i < def__SIZE_; ++i)
 			{
 				_ptr.emplace_back(&v_Obj[i]);
 			}
 			radixSort(_ptr);
-			if (mP < _ptr[11]->score)
-			{
-				mI = _ptr[11]->i;
-				mP = _ptr[11]->score;
-			}
 			mA_gen(_ptr, 3);
 
 			for (size_t i = 0; i < v_Obj.size(); ++i)
 			{
-				v_Obj[i].obj.setPosition(static_cast<float>(POSX), static_cast<float>(POSY));
+				v_Obj[i].obj.setPosition(static_cast<float>(def_POSX), static_cast<float>(def_POSY));
 				v_Obj[i].life = true;
 				v_Obj[i].score = 0;
 			}
-			maxposx_obj = POSX;
+			maxposx_obj = def_POSX;
 			v_Wll.clear();
 			target.clear();
 			tmpy = nndx::randT() % 4;
 			wposx = 470.0f;
-			target.emplace_back(POSY_WALL + 20 + tmpy * 20);
-			v_Wll.push_back(::std::vector<Wall>());
-			v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL));
-			for (int i = 0; i < _NUM_ACTIVE_WALL; ++i)
+			target.emplace_back(def_POSY_WALL + 20 + tmpy * 20);
+			v_Wll.emplace_back(::std::vector<Wall>());
+			v_Wll.back().reserve(6);
+			v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL));
+			for (int i = 0; i < def__NUM_ACTIVE_WALL; ++i)
 			{
-				if (i != tmpy)	v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL + 20 + i * 20));
+				if (i != tmpy)	v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL + 20 + i * 20));
 			}
-			v_Wll.back().emplace_back(t, wposx, static_cast<float>(POSY_WALL + 100));
+			v_Wll.back().emplace_back(t, wposx, static_cast<float>(def_POSY_WALL + 100));
 			wposx += 20.0f;
 			tm.restart();
 		}
@@ -276,34 +347,7 @@ int main()
 		win.display();
 	}
 
-	for (int i = 0; i < _SIZE_; ++i)
-	{
-		::std::string s = "output/net_";
-		char ch;
-		if (i < 10)
-		{
-			ch = '0' + i;
-			s += ch;
-			s += ".txt";
-		}
-		else
-		{
-			s += '1';
-			ch = '0' + (i % 10);
-			s += ch;
-			s += ".txt";
-		}
-		v_Obj[i].net.saveF(s);
-	}
-
-	::std::ofstream out("output/log.txt");
-	for (int i = 0; i < _SIZE_; ++i)
-	{
-		out << "net_" << i << " - " << v_Obj[i].score << ::std::endl;
-	}
-	out.close();
-
-	return 0;
+	win.setActive(false);
 }
 
 void radixSort(::std::vector<Object*>& a)
